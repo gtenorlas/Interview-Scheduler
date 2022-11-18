0568ba1bcebebe35ec50862fe5f7b6b569ef3874
Gene Tenorlas <gene.tenorlas@yahoo.com>
2022-11-18 18:04:17 -0500

    Update Appointment to use EMPTY and SHOW

---
+++ .history/src/components/Appointment/index_20221118180212.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles.scss'
+import Header from './Header'
+import Show from './Show'
+import Empty from './Empty'
+import useVisualMode from 'hooks/useVisualMode'
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
+export default function Appointment (props) {
+const  { mode, transition, back }  = useVisualMode(props.interview ? SHOW : EMPTY)
+
+  return (
+    <article className='appointment'>
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+{mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />
+)}
+    </article>
+  )
+}
