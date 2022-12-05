2bc2f65d871b2b36bf50f7d9c19066d38506f01e
Gene Tenorlas <gene.tenorlas@yahoo.com>
2022-12-05 14:44:39 -0500

    Create appointments.spec.js

--- .history/cypress/integration/navagation.spec_20221204234157.js
+++ .history/cypress/integration/navagation.spec_20221204234157.js
@@ -5,8 +5,6 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday",()=>{
     cy.visit("/")
-    cy.contains("li", "Tuesday")
-    .click()
-    .should("have.css", "background-color", "rgb(242, 242, 242)");
+    cy.get("li").contains("Tuesday").click();
   })
 });
